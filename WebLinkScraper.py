a8ac494775ecaf51893b2b27d2b3c0ef407d782b
Isaac Yauk <isaacyauk@yahoo.com>
2024-01-20 07:15:34 -0700

    adds steps to enter search term and detect link results

--- WebLinkScraper.py
+++ WebLinkScraper.py
@@ -1,8 +1,14 @@
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 
 driver = webdriver.Chrome()
 
 driver.get("https://google.com")
 
-driver.find_element()
+# Search Query and "ENTER" keypress
+driver.find_element(by=By.CSS_SELECTOR, value="#APjFqb").send_keys("Dogs")
+driver.find_element(by=By.CSS_SELECTOR, value="#APjFqb").send_keys(Keys.ENTER)
 
+# location of weblink
+driver.find_element(by=By.XPATH, value="//cite").text()
