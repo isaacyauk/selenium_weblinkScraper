f5939a2edc36a4a6604aec099d2419b98e1142c4
Isaac Yauk <isaacyauk@yahoo.com>
2024-02-29 15:35:07 -0700

    closes issue #2

--- WebLinkScraper.py
+++ WebLinkScraper.py
@@ -12,19 +12,36 @@ driver = webdriver.Chrome()
 
 # driver = webdriver.Edge()
 
-def scroll(times):
-    # Define how many times to scroll (you can adjust this number)
+
+# Define how many times to scroll (you can adjust this number)
+def scrollToResultsUntil(duration, times):
+    for i in range(times):
+
+        try:
+            # Find the element you want to scroll to
+            element = driver.find_element(by=By.XPATH, value="//span[text()='More results']")
+            start_time = time.time()
+
+            while time.time() - start_time < duration:
+                # Execute JavaScript to scroll the page down to the element
+                driver.execute_script("arguments[0].scrollIntoView();", element)
+
+            element.click()
+        except:
+            break
+
+
+
+
 
     # Scroll to the bottom of the page
-    for _ in range(times):
-        driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-
-
-def click(times):
-    for _ in range(times):
-        time.sleep(1)
-        wait.until(EC.element_to_be_clickable((By.XPATH, "//div//span[contains(., 'More results')]")))
-        driver.find_element(by=By.XPATH, value="//div//span[contains(., 'More results')]").click()
+    # for i in range(times):
+    #     driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+    #     time.sleep(2)
+    #
+    #     if 6 == times:
+    #         wait.until(EC.element_to_be_clickable((By.XPATH, "//div//span[contains(., 'More results')]")))
+    #
 
 
 driver.get("https://google.com")
@@ -33,19 +50,16 @@ driver.get("https://google.com")
 driver.find_element(by=By.CSS_SELECTOR, value="#APjFqb").send_keys(search_term)
 driver.find_element(by=By.CSS_SELECTOR, value="#APjFqb").send_keys(Keys.ENTER)
 
-#wait = WebDriverWait(driver, 10)
-#wait.until(EC.visibility_of_element_located((By.XPATH, "//div//span[contains(., 'More results')]")))
-
-
 wait = WebDriverWait(driver, 10)
-searchResults = wait.until(EC.presence_of_all_elements_located((By.XPATH, "//div//cite")))
-
-wait.until(EC.element_to_be_clickable((By.XPATH, "//div//span[contains(., 'More results')]"))).click()
+searchResults = wait.until(EC.presence_of_all_elements_located((By.XPATH, "//div[@role='main']//span//a[@jsname]"))) # USE THE .HREF javascript element here!!
 
 # location of weblink
 # searchResults = driver.find_elements(by=By.XPATH, value="//div//cite")
-click(7)
+#scrollToMoreResults()
 # this loop looks at all the "Cite" results on the DOM and checks for if it is a link or not.
+
+scrollToResultsUntil(2, 200)
+
 for result in searchResults:
     link = result.text
 
@@ -55,7 +69,6 @@ for result in searchResults:
     # thing = driver.find_element(by=By.XPATH, value="//cite").text
 
 
-moretest = driver.find_element(By.XPATH, value="//div//span[contains(., 'More results')]").click()
 
 print("end of thing")
 
