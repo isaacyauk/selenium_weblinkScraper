3bd54155e7046cc9d62efb532f592b5221fb402b
Isaac Yauk <isaacyauk@yahoo.com>
2024-03-01 14:11:00 -0700

    updates import statements

--- Main.py
+++ Main.py
@@ -2,7 +2,6 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from ChromeDriverMethods import ChromeDriverMethods
-import time
 
 search_term = input("Enter desired search term: ")
 
